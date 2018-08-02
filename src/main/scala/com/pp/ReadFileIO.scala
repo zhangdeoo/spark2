2bfd0ef5e27b48167d24a79058317f38ee098054
zhanglingjun <861724927@qq.com>
2018-08-02 15:09:27 +0800

    scala 读文件和写文件

--- src/main/scala/com/pp/ReadFileIO.scala
+++ src/main/scala/com/pp/ReadFileIO.scala
@@ -1,5 +1,6 @@
 package com.pp
 
+import java.io.{File, PrintWriter}
 import java.util
 
 import scala.io.Source
@@ -8,43 +9,18 @@ import scala.io.Source
   * Created by Administrator on 2018/5/31.
   */
 object ReadFileIO {
-  val mapscope_truncate = new util.HashMap[String, Int]()
 
   def main(args: Array[String]): Unit = {
-    val fileName = "E:\\01_workspcace\\02_IDEAworkspace\\data-audit\\data-audit-spark\\src\\main\\resources\\file\\ip_20170905115730.txt"
+    val fileName = "C:\\t.txt"
     val source = Source.fromFile(fileName)
     val lines = source.getLines
-    //
-    var first_china = true
-    var continuous_china = true
-    var index = 0
-    var contins_china = 0
+    val writer = new PrintWriter(new File("C:\\learningScala.txt"))
     for (line <- lines) {
-      val arr = line.split(",", -1)
-      val startIpNum = arr(0)
-      val stopIpNum = arr(1)
-      if (line.contains("中国")) {
-        if (first_china) {
-          first_china = false
-          mapscope_truncate.put(startIpNum, index)
-          mapscope_truncate.put(stopIpNum, index)
-        } else {
-          if (continuous_china) {
-            mapscope_truncate.put(stopIpNum, index)
-          } else {
-            mapscope_truncate.put(startIpNum + "#discontinuity", index)
-            mapscope_truncate.put(stopIpNum, index)
-          }
-        }
-        continuous_china = true
-      } else {
-        continuous_china = false
-      }
-      //索引号自增
-      index += 1
+      val linearr=line.split(" ",-1)
+      val newline=linearr(0)+"\t"+linearr(1)+""+linearr(2)
+      writer.println(newline)
     }
-    println(mapscope_truncate.size())
-    println(mapscope_truncate)
+
     source.close; //记得要关闭source
   }
 
